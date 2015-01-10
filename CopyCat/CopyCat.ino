8492729b301a887d9a1caa64d6779b3d3f8191a5
Dylan Vodzak <dvodzak16@punahou.edu>
2015-01-09 20:13:32 -1000

    Final Final Beta Complete

--- CopyCat/CopyCat.ino
+++ CopyCat/CopyCat.ino
@@ -1,3 +1,11 @@
+/*
+Objective:
+Copy the red fairy by holding down the corresponding buttons.
+IMPORTANT:
+You must hold down the button until the red fairy changes so the note can play.
+
+*/
+
 #include <MeggyJrSimple.h>    // Required code, line 1 of 2.
 
 boolean alive = true;
@@ -23,7 +31,80 @@ void deathScreen()
   
 }
 
-void ButtonsNeutral()
+void dotChecker()
+{
+  CheckButtonsDown();      //When "example" button is hit, play note
+  
+  if(Button_Left)
+  {
+    Tone_Start(ToneG5, 100);
+    DrawPx(1,4,FullOn);
+    if(ReadPx(1,4) == Red)
+    {
+      Tone_Start(ToneG6, 100);
+    }
+   DisplaySlate();
+  }
+  
+   if(Button_Right)
+  {
+    Tone_Start(ToneA5, 100);
+    DrawPx(3,4,FullOn);
+    if(ReadPx(3,4) == Red)
+    {
+      Tone_Start(ToneA5, 100);
+    }
+    DisplaySlate();
+  }
+  
+   if(Button_Up)
+  {
+    Tone_Start(ToneB5, 100);
+    DrawPx(2,5,FullOn);
+    if(ReadPx(2,5) == Red)
+    {
+      Tone_Start(ToneB5, 100);
+    }
+   DisplaySlate();
+  }
+  
+   if(Button_Down)
+  {
+    Tone_Start(ToneD6, 100);
+    DrawPx(2,3,FullOn);
+    if(ReadPx(2,3) == Red)
+    {
+      Tone_Start(ToneD5, 100);
+    }
+   DisplaySlate();
+  }
+  
+   if(Button_A)
+  {
+    Tone_Start(ToneE5, 100);
+    DrawPx(6,4,FullOn);
+    if(ReadPx(6,4) == Red)
+    {
+      Tone_Start(ToneE5, 100);
+    }
+   DisplaySlate();
+  }
+  
+   if(Button_B)
+  {
+    Tone_Start(ToneF5, 100);
+    DrawPx(5,4,FullOn);
+    if(ReadPx(5,4) == Red)
+    {
+      Tone_Start(ToneF5, 100);
+    }
+   DisplaySlate();
+  }
+  
+}
+
+
+void ButtonsNeutral()      // Dots when none are lit
 {
   DrawPx(5,4,DimBlue);
   DrawPx(6,4,DimBlue);
@@ -34,7 +115,7 @@ void ButtonsNeutral()
   DisplaySlate();
 }
 
-void lightAButton()
+void lightAButton()      // Dots when only A button is lit
   {
   DrawPx(6,4,Red);
   DrawPx(5,4,DimBlue);
@@ -42,11 +123,12 @@ void lightAButton()
   DrawPx(3,4,DimBlue);
   DrawPx(2,5,DimBlue);
   DrawPx(2,3,DimBlue);
-    DisplaySlate();
-    delay(200);
+  DisplaySlate();
+  delay(500);
+    
   }
   
-void lightBButton()
+void lightBButton()   // Dots when only B button is lit
   {
   DrawPx(5,4,Red);
   DrawPx(6,4,DimBlue);
@@ -55,10 +137,10 @@ void lightBButton()
   DrawPx(2,5,DimBlue);
   DrawPx(2,3,DimBlue);
     DisplaySlate();
-    delay(200);
+    
   }
   
-void lightRightButton()
+void lightRightButton()  //Dots when only Right button is lit
   {
   DrawPx(5,4,DimBlue);
   DrawPx(6,4,DimBlue);
@@ -67,10 +149,10 @@ void lightRightButton()
   DrawPx(2,5,DimBlue);
   DrawPx(2,3,DimBlue);
     DisplaySlate();
-    delay(200);
+   
   }
 
-void lightLeftButton()
+void lightLeftButton()  //Dots only when Left button is lit
   {
   DrawPx(5,4,DimBlue);
   DrawPx(6,4,DimBlue);
@@ -81,7 +163,7 @@ void lightLeftButton()
     DisplaySlate();
   }
 
-void lightUpButton()
+void lightUpButton()    //Dots only when Up button is lit
   {
   DrawPx(5,4,DimBlue);
   DrawPx(6,4,DimBlue);
@@ -90,10 +172,9 @@ void lightUpButton()
   DrawPx(3,4,DimBlue);
   DrawPx(2,3,DimBlue);
     DisplaySlate();
-    delay(200);
   }
 
-void lightDownButton()
+void lightDownButton()   //Dots only when Down button is lit
   {
   
   DrawPx(1,4,DimBlue);
@@ -105,84 +186,153 @@ void lightDownButton()
   DisplaySlate();
  
   }
-  
-//----------------------------------
 
-void aButton()
-  {
-    CheckButtonsDown();
-    if(Button_A){
-    DrawPx(6,4,FullOn);
-    DisplaySlate();
-  }
-  }
 
-void bButton()
+
+void loop()          // Loop has red dot order for "Mary Had a Little Lamb"
 {
-  CheckButtonsDown();
-  if(Button_B){
-  DrawPx(5,4,FullOn);
-  DisplaySlate();
-}
-}
-
-void leftButton()
-{
-  CheckButtonsDown();
-  if(Button_Left){
-  DrawPx(1,4,FullOn);
-  DisplaySlate();
-}
-}
-
-void rightButton()
-{
-  CheckButtonsDown();
-  if(Button_Right){
-  DrawPx(3,4,FullOn);
-  DisplaySlate();
-}
-}
-
-void upButton()
-{
-  CheckButtonsDown();
-  if(Button_Up){
-  DrawPx(2,5,FullOn);
-  DisplaySlate();
-  }
-}
-
-void downButton()
-{
-  CheckButtonsDown();
-  if(Button_Down){
-  DrawPx(2,3,FullOn);
-  DisplaySlate();
-  }
-}
-
-
-void loop()
-{
-  CheckButtonsPress();
+  dotChecker();
   ButtonsNeutral();
- 
-  if(Button_Down){
-    while(lightDownButton)
-    {
-     ClearSlate();
-     ButtonsNeutral();
-     DisplaySlate();
-    }
-  }
-  else{
-    delay(2000);
-    ClearSlate();
-    deathScreen();
-  }
-  
+  delay(1000);
+  dotChecker();
+  ButtonsNeutral();
+  lightUpButton();
+  delay(1000);
+  dotChecker();
+  ButtonsNeutral();
+  ClearSlate();
+  dotChecker();
+  ButtonsNeutral();
+  lightRightButton();
+  delay(1000);
+  dotChecker();
+  ButtonsNeutral();
+  ClearSlate();
+  lightLeftButton();
+  delay(1000);
+  dotChecker();
+  ButtonsNeutral();
+  ClearSlate();
+  lightRightButton();
+  delay(1000);
+  dotChecker();
+  ButtonsNeutral();
+  ClearSlate();
+  lightUpButton();
+  delay(1000);
+  dotChecker();
+  ButtonsNeutral();
+  ClearSlate();
+  lightUpButton();
+  delay(1000);
+  dotChecker();
+  ButtonsNeutral();
+  ClearSlate();
+  lightUpButton();
+  delay(1000);
+  dotChecker();
+  ButtonsNeutral();
+  ClearSlate();
+  lightRightButton();
+  delay(1000);
+  dotChecker();
+  ButtonsNeutral();
+  ClearSlate();
+  lightRightButton();
+  delay(1000);
+  dotChecker();
+  ButtonsNeutral();
+  ClearSlate();
+  lightRightButton();
+  delay(1000);
+  dotChecker();
+  ButtonsNeutral();
+  ClearSlate();
+  lightUpButton();
+  delay(1000);
+  dotChecker();
+  ButtonsNeutral();
+  ClearSlate();
+  lightDownButton();
+  delay(1000);
+  dotChecker();
+  ButtonsNeutral();
+  ClearSlate();
+  lightDownButton();
+  delay(1000);
+  dotChecker();
+  ButtonsNeutral();
+  ClearSlate();
+  lightUpButton();
+  delay(1000);
+  dotChecker();
+  ButtonsNeutral();
+  ClearSlate();
+  lightRightButton();
+  delay(1000);
+  dotChecker();
+  ButtonsNeutral();
+  ClearSlate();
+  lightLeftButton();
+  delay(1000);
+  dotChecker();
+  ButtonsNeutral();
+  ClearSlate();
+  lightRightButton();
+  delay(1000);
+  dotChecker();
+  ButtonsNeutral();
+  ClearSlate();
+  lightUpButton();
+  delay(1000);
+  dotChecker();
+  ButtonsNeutral();
+  ClearSlate();
+  lightUpButton();
+  delay(1000);
+  dotChecker();
+  ButtonsNeutral();
+  ClearSlate();
+  lightUpButton();
+  delay(1000);
+  dotChecker();
+  ButtonsNeutral();
+  ClearSlate();
+  lightUpButton();
+  delay(1000);
+  dotChecker();
+  ButtonsNeutral();
+  ClearSlate();
+  lightRightButton();
+  delay(1000);
+  dotChecker();
+  ButtonsNeutral();
+  ClearSlate();
+  lightRightButton();
+  delay(1000);
+  dotChecker();
+  ButtonsNeutral();
+  ClearSlate();
+  lightUpButton();
+  delay(1000);
+  dotChecker();
+  ButtonsNeutral();
+  ClearSlate();
+  lightRightButton();
+  delay(1000);
+  dotChecker();
+  ButtonsNeutral();
+  ClearSlate();
+  lightLeftButton();
+  delay(1000);
+  dotChecker();
+  ButtonsNeutral();
+  ClearSlate();
   DisplaySlate();
+  delay(5000);
+  
+  
+
   }
   
   
