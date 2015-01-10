4f683f943e29067ebd803c4c88b6e72207375661
Dylan Vodzak <dvodzak16@punahou.edu>
2015-01-09 14:28:41 -1000

    Added Voids for different Button states

--- CopyCat/CopyCat.ino
+++ CopyCat/CopyCat.ino
@@ -1,5 +1,6 @@
 #include <MeggyJrSimple.h>    // Required code, line 1 of 2.
- 
+
+int x = 0;
 
  void setup()                    // run once, when the sketch starts
  {
@@ -9,69 +10,183 @@
 
 }                            // End setup()
 
-int x = random(5);
-
-void Draw()
-{
-if(x==0)
-    {
-      DrawPx(1,4,Red);
-    }
- 
- struct Point        // Defined struct
-{
-  int x; 
-  int y;
-};
-
-Point s1 = {1,4};    //Defines points in array 
-Point s2 = {2,3};
-Point s3 = {2,5};
-Point s4 = {3,4};
-Point s5 = {5,4};
-Point s6 = {6,4};
-Point lightArray[6] = {s1, s2, s3, s4, s5, s6};
+void deathScreen()
+{      
+  for(int i = 0; i < 8; i++)       //Easier way of making the screen red, credit to Mr. Kiang
+      {
+        for(int x = 0; x < 8; x++)
+        {
+          DrawPx(i,x,1); //Draws the colors out
+        }
+      }
+  DisplaySlate(); // Diplsays slate
+  
 }
+
+void ButtonsNeutral()
+{
+  DrawPx(5,4,DimBlue);
+  DrawPx(6,4,DimBlue);
+  DrawPx(1,4,DimBlue);
+  DrawPx(3,4,DimBlue);
+  DrawPx(2,5,DimBlue);
+  DrawPx(2,3,DimBlue);
+  DisplaySlate();
+}
+
+void lightAButton()
+  {
+  DrawPx(6,4,Red);
+  DrawPx(5,4,DimBlue);
+  DrawPx(1,4,DimBlue);
+  DrawPx(3,4,DimBlue);
+  DrawPx(2,5,DimBlue);
+  DrawPx(2,3,DimBlue);
+    DisplaySlate();
+    delay(200);
+  }
+  
+void lightBButton()
+  {
+  DrawPx(5,4,Red);
+  DrawPx(6,4,DimBlue);
+  DrawPx(1,4,DimBlue);
+  DrawPx(3,4,DimBlue);
+  DrawPx(2,5,DimBlue);
+  DrawPx(2,3,DimBlue);
+    DisplaySlate();
+    delay(200);
+  }
+  
+void lightRightButton()
+  {
+  DrawPx(5,4,DimBlue);
+  DrawPx(6,4,DimBlue);
+  DrawPx(1,4,DimBlue);
+  DrawPx(3,4,Red);
+  DrawPx(2,5,DimBlue);
+  DrawPx(2,3,DimBlue);
+    DisplaySlate();
+    delay(200);
+  }
+
+void lightLeftButton()
+  {
+  DrawPx(5,4,DimBlue);
+  DrawPx(6,4,DimBlue);
+  DrawPx(1,4,Red);
+  DrawPx(3,4,DimBlue);
+  DrawPx(2,5,DimBlue);
+  DrawPx(2,3,DimBlue);
+    DisplaySlate();
+  }
+
+void lightUpButton()
+  {
+  DrawPx(5,4,DimBlue);
+  DrawPx(6,4,DimBlue);
+  DrawPx(2,5,Red);
+  DrawPx(1,4,DimBlue);
+  DrawPx(3,4,DimBlue);
+  DrawPx(2,3,DimBlue);
+    DisplaySlate();
+    delay(200);
+  }
+
+void lightDownButton()
+  {
+  DrawPx(1,4,DimBlue);
+  DrawPx(3,4,DimBlue);
+  DrawPx(2,3,Red);
+  DrawPx(5,4,DimBlue);
+  DrawPx(6,4,DimBlue);
+  DrawPx(2,5,DimBlue);
+  
+  
+    DisplaySlate();
+    delay(200);
+  }
+  
+//----------------------------------
 
 void aButton()
   {
-    DrawPx(5,4,White);
+    CheckButtonsDown();
+   
+    if(Button_A){
+    DrawPx(6,4,FullOn);
     DisplaySlate();
+    
+  }
   }
 
 void bButton()
 {
-  DrawPx(6,4,White);
+  CheckButtonsDown();
+  if(Button_B){
+  DrawPx(5,4,FullOn);
   DisplaySlate();
+}
 }
 
 void leftButton()
 {
-  DrawPx(1,4,White);
+  CheckButtonsDown();
+  if(Button_Left){
+  DrawPx(1,4,FullOn);
   DisplaySlate();
+}
 }
 
 void rightButton()
 {
-  DrawPx(3,4,White);
+  CheckButtonsDown();
+  if(Button_Right){
+  DrawPx(3,4,FullOn);
   DisplaySlate();
+}
 }
 
 void upButton()
 {
-  DrawPx(2,5,White);
+  CheckButtonsDown();
+  if(Button_Up){
+  DrawPx(2,5,FullOn);
   DisplaySlate();
+  }
 }
 
 void downButton()
 {
-  DrawPx(2,3,White);
+  CheckButtonsDown();
+  if(Button_Down){
+  DrawPx(2,3,FullOn);
   DisplaySlate();
+  }
 }
 
 
 void loop()
 {
+  CheckButtonsPress();
+  ButtonsNeutral();
+  delay(2000);
+  lightDownButton();
+  if(Button_Down)
+  {
+    Tone_Start(ToneG6, 500);
+    Tone_Update();
+    ClearSlate();
+    ButtonsNeutral();
+    DisplaySlate();
+  }
+  else
+  {
+    delay(5000);
+    ClearSlate();
+    deathScreen();
+  }
+  
   aButton();
   bButton();
   upButton();
@@ -79,7 +194,8 @@ void loop()
   leftButton();
   rightButton();
   DisplaySlate();
-  CheckButtonsDown();
+  
+  
   
   
 }
